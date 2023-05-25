f58e9d09542453011ad5adc15768059ad3f85ce2
Samuel Roberto <samuel.roberto997@gmail.com>
2023-05-25 15:16:05 +0200

    chore: project structure with cli and commands

---
+++ src/commands/init.ts
@@ -0,0 +1,39 @@
+import * as fs from "fs";
+import { ask } from "../utils/functions";
+
+export const init = async (): Promise<void> => {
+  let does_override = true;
+
+  if (fs.existsSync("./.cosmofactory.json")) {
+    does_override = (
+      await ask({
+        type: "toggle",
+        name: "does_override",
+        message:
+          ".cosmofactory configuration file already exists, do you want to override it ?",
+        active: "yes",
+        inactive: "no",
+      })
+    ).does_override;
+  }
+
+  if (does_override) {
+    // create config file
+    fs.writeFileSync(
+      "./.cosmofactory.json",
+      JSON.stringify(
+        {
+          files: {
+            "package.json": "./",
+            "plugin-tailwind.ts": "./",
+            "tailwind.config.ts": "./",
+          },
+          tailwind: true,
+        },
+        null,
+        2
+      )
+    );
+    console.log(".cosmofactory.json file was successfully created");
+  }
+};
---
+++ src/utils/validation/cosmofactory-config.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const cosmofactory_config_schema = z.object({
+  files: z.any(),
+  tailwind: z.boolean(),
+});
